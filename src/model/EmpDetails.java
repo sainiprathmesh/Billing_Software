ad5d966b34c2ca56129adf07911d0509ea50942a
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-08-18 10:06:08 +0530

    setter method for all these variables is created

--- src/model/EmpDetails.java
+++ src/model/EmpDetails.java
@@ -19,4 +19,17 @@ public class EmpDetails {
     public String getPhno() {
         return phno;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhno(String phno) {
+        this.phno = phno;
+    }
+
 }
