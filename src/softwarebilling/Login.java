95fa760c770135f0b63a8149de62ee7089c80789
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-08-19 22:11:17 +0530

    for activating GUI within try-catch.

--- src/softwarebilling/Login.java
+++ src/softwarebilling/Login.java
@@ -124,7 +124,22 @@ public class Login extends javax.swing.JFrame {
     }
 
     public static void main(String[] args) {
-
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
     }
 
     private JTextField jTextField1;
