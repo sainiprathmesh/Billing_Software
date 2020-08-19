147ff4b6365e697b1e958c4f0c8f27884c6d7385
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-08-19 20:24:22 +0530

    all logic written in main method within try-catch statement.

--- src/softwarebilling/StartProject.java
+++ src/softwarebilling/StartProject.java
@@ -66,6 +66,22 @@ public class StartProject extends javax.swing.JFrame {
 
     public static void main(String[] args) {
 
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(StartProject.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(StartProject.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(StartProject.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(StartProject.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
     }
 
     private void fromWidowOpened(java.awt.event.WindowEvent evt) {
