d8298613052b46b39ed7d063731507847df7f737
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-23 22:01:59 +0530

    create object for textfield2

--- src/admin/ChangePwd.java
+++ src/admin/ChangePwd.java
@@ -8,10 +8,21 @@ public class ChangePwd {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
 
     private void initComponents() {
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
 
     }
