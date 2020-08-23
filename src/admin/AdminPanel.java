7a42c8774037a7684a6e10797de92624674e6a91
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-08-23 12:02:12 +0530

    Use try catch for exception

--- src/admin/AdminPanel.java
+++ src/admin/AdminPanel.java
@@ -29,9 +29,7 @@ public class AdminPanel extends javax.swing.JFrame {
         initComponents();
     }
 
-    public static void main(String args[]) {
 
-    }
 
     //@SuppressWarnings("unchecked")
     private void initComponents() {
@@ -72,6 +70,29 @@ public class AdminPanel extends javax.swing.JFrame {
 
         }
 
+
+    }
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(AdminPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(AdminPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(AdminPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(AdminPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
 
     }
 
