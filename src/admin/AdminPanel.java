e871e16104e5d527a7a417bfa7e625b453c99e5a
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-08-23 12:03:17 +0530

    create method run()

--- src/admin/AdminPanel.java
+++ src/admin/AdminPanel.java
@@ -93,6 +93,11 @@ public class AdminPanel extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new AdminPanel().setVisible(true);
+            }
+        });
 
     }
 
