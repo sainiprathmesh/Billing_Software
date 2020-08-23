2fb4dda048493cbce3f4ed87d7658109570774fa
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-08-23 12:45:22 +0530

    add method jButton3ActionPerformed().

--- src/admin/AdminPanel.java
+++ src/admin/AdminPanel.java
@@ -96,6 +96,15 @@ public class AdminPanel extends javax.swing.JFrame {
 
     }//GEN-LAST:event_jButton5ActionPerformed
 
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+
+        // AddItem ai=new AddItem();
+        //ai.setVisible(true);
+        setVisible(false);
+
+    }//GEN-LAST:event_jButton3ActionPerformed
+
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
 
