579a05c68fa958b225e939af532d44f00d32d687
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-08-23 11:31:33 +0530

    Declare variables for panel, button and Label.

--- src/admin/AdminPanel.java
+++ src/admin/AdminPanel.java
@@ -1,11 +1,28 @@
 package admin;
 
+import javax.swing.*;
+
 /**
  * @author Latesh on 8/23/2020 at 11:05 AM
  **/
 
 
 public class AdminPanel extends javax.swing.JFrame {
+    private JPanel jPanel1;
+    private JButton jButton5;
+    private JLabel jLabel1;
+    private JPanel jPanel2;
+    private JButton jButton1;
+    private JButton jButton2;
+    private JLabel jLabel2;
+    private JLabel jLabel3;
+    private JPanel jPanel3;
+    private JButton jButton3;
+    private JButton jButton4;
+    private JLabel jLabel4;
+    private JLabel jLabel5;
+    private JLabel jLabel6;
+    private JLabel jLabel7;
 
     public AdminPanel() {
         initComponents();
@@ -13,6 +30,7 @@ public class AdminPanel extends javax.swing.JFrame {
 
     //@SuppressWarnings("unchecked")
     private void initComponents() {
+
 
     }
 }
