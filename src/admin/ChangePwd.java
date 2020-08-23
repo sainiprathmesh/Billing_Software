ccd116d6fe2f5d83de45d49f031395ee858574cb
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-24 01:56:41 +0530

    class inherited from javax.swing.JFrame

--- src/admin/ChangePwd.java
+++ src/admin/ChangePwd.java
@@ -1,9 +1,13 @@
 package admin;
-public class ChangePwd {
+public class ChangePwd extends javax.swing.JFrame {
     public ChangePwd() {
         initComponents();
     }
 
+    private javax.swing.JLabel jLabel1;
+
     private void initComponents() {
+        jLabel1 = new javax.swing.JLabel();
     }
+
 }
