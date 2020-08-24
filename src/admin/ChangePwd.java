f9aa707da3e1887f65a1ffdef51820f8efc0b023
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-24 07:59:32 +0530

    create object for Textfield3

--- src/admin/ChangePwd.java
+++ src/admin/ChangePwd.java
@@ -7,11 +7,13 @@ public class ChangePwd extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
     }
 
 }
