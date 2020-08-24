afa10f6d7aa98251071d7a8143cf2f78ea8d7f9f
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-24 08:02:06 +0530

    create object for Button1

--- src/admin/ChangePwd.java
+++ src/admin/ChangePwd.java
@@ -8,12 +8,14 @@ public class ChangePwd extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
+    private javax.swing.JButton jButton1;
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jTextField2 = new javax.swing.JTextField();
         jTextField3 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
     }
 
 }
