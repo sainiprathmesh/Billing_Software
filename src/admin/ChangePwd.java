6d496de82081fc02345d480e517d224a254afd84
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-24 12:25:47 +0530

    setText() method called is used to set the current text for TextField3

--- src/admin/ChangePwd.java
+++ src/admin/ChangePwd.java
@@ -123,7 +123,8 @@ public class ChangePwd extends javax.swing.JFrame {
                     JOptionPane.showMessageDialog(rootPane, "Password updated successfully");
                     jTextField1.setText("");
                     jTextField2.setText("");
-
+                    jTextField3.setText("");
+                }
                 }
 
 
