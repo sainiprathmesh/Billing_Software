b3a4ffc2f8e6c374f0c10d0ae0ece8f2c79a6f94
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-24 08:40:58 +0530

    some code for Button1

--- src/admin/ChangePwd.java
+++ src/admin/ChangePwd.java
@@ -33,6 +33,11 @@ public class ChangePwd extends javax.swing.JFrame {
             }
         });
         jButton1.setText("Change Password");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
 
     }
