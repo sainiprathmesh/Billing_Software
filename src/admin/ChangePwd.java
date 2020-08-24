ceb599957645832a746c308c160a89ba31cf5176
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-24 08:03:19 +0530

    create object for Label2

--- src/admin/ChangePwd.java
+++ src/admin/ChangePwd.java
@@ -9,6 +9,7 @@ public class ChangePwd extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel2;
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
@@ -16,6 +17,7 @@ public class ChangePwd extends javax.swing.JFrame {
         jTextField2 = new javax.swing.JTextField();
         jTextField3 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
     }
 
 }
