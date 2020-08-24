e911f62b818269c82d4f456e78c24eb48f4148c0
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-24 12:50:33 +0530

    catch block  added for Handle errors

--- src/admin/ChangePwd.java
+++ src/admin/ChangePwd.java
@@ -33,17 +33,18 @@ public class ChangePwd extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(ChangePwd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
             java.util.logging.Logger.getLogger(ChangePwd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        private void initComponents () {
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
 
-            jLabel1 = new javax.swing.JLabel();
-            jTextField1 = new javax.swing.JTextField();
-            jTextField2 = new javax.swing.JTextField();
-            jTextField3 = new javax.swing.JTextField();
-            jButton1 = new javax.swing.JButton();
-            jLabel2 = new javax.swing.JLabel();
-            jLabel3 = new javax.swing.JLabel();
-            jLabel4 = new javax.swing.JLabel();
+            private void initComponents () {
+
+                jLabel1 = new javax.swing.JLabel();
+                jTextField1 = new javax.swing.JTextField();
+                jTextField2 = new javax.swing.JTextField();
+                jTextField3 = new javax.swing.JTextField();
+                jButton1 = new javax.swing.JButton();
+                jLabel2 = new javax.swing.JLabel();
+                jLabel3 = new javax.swing.JLabel();
+                jLabel4 = new javax.swing.JLabel();
 
                 setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
                 jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18));
