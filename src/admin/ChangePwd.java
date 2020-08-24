b8b0ab5e9e5a9fc09fe7c476677cb2399deccaff
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-24 10:06:32 +0530

    add components on jTextField2

--- src/admin/ChangePwd.java
+++ src/admin/ChangePwd.java
@@ -71,6 +71,7 @@ public class ChangePwd extends javax.swing.JFrame {
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                         .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
 
 
     }
