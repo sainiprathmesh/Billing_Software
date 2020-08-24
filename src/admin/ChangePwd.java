a65de858ca647707166add004c956478ba84f0f6
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-24 12:51:03 +0530

    getLogger() method called and import java.util.logging

--- src/admin/ChangePwd.java
+++ src/admin/ChangePwd.java
@@ -34,7 +34,8 @@ public class ChangePwd extends javax.swing.JFrame {
         } catch (IllegalAccessException ex) {
             java.util.logging.Logger.getLogger(ChangePwd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-
+            java.util.logging.Logger.getLogger(ChangePwd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
             private void initComponents () {
 
                 jLabel1 = new javax.swing.JLabel();
