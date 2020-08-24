17c263f970ca726be402229a2ba548f9629f89ff
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-24 10:39:14 +0530

    call setVisible(false) method .

--- src/admin/ChangePwd.java
+++ src/admin/ChangePwd.java
@@ -87,6 +87,7 @@ public class ChangePwd extends javax.swing.JFrame {
         private void jLabel1MouseClicked (java.awt.event.MouseEvent evt){//GEN-FIRST:event_jLabel1MouseClicked
             AdminPanel ap = new AdminPanel();
             ap.setVisible(true);
+            setVisible(false);
 
 
         }
