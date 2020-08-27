d03d49557e8d24c37a5ac1c147c5da7e375ad2a5
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-27 15:48:03 +0530

    void mouseClicked() invoked

--- src/admin/RegisterEmployee.java
+++ src/admin/RegisterEmployee.java
@@ -22,8 +22,9 @@ public class RegisterEmployee extends javax.swing.JFrame {
         jLabel1.setText("Back");
         jLabel1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
 
+
+            }
 
         }
-
-    }
