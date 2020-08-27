745a3c294be081a167af0be84db01d7828bdf0ff
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-27 16:30:39 +0530

    addComponents on jPasswordField1

--- src/admin/RegisterEmployee.java
+++ src/admin/RegisterEmployee.java
@@ -75,6 +75,8 @@ public class RegisterEmployee extends javax.swing.JFrame {
                                         .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addComponent(jLabel3))
                                 .addGap(40, 40, 40)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jPasswordField1, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
 
 
     }
