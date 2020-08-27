75571af935ba296167451fb17bc87d33e0ea05c8
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-27 15:41:06 +0530

    some code for  foreground and text on jLabel1

--- src/admin/RegisterEmployee.java
+++ src/admin/RegisterEmployee.java
@@ -18,6 +18,9 @@ public class RegisterEmployee extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18));
+        jLabel1.setForeground(new java.awt.Color(255, 0, 0));
+        jLabel1.setText("Back");
+        jLabel1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
 
 
     }
