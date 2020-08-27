7bf1ee0cb68673a427ee742ceedefc83f7e05837
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-27 17:16:58 +0530

    print occur Exception

--- src/admin/RegisterEmployee.java
+++ src/admin/RegisterEmployee.java
@@ -154,8 +154,16 @@ public class RegisterEmployee extends javax.swing.JFrame {
                     } else {
                         JOptionPane.showMessageDialog(rootPane, "Employee not registered due to some error", "Error", JOptionPane.ERROR_MESSAGE);
                     }
-                    con.close();
+                    con.close();catch(Exception e)
+                    {
+                        System.out.println(e);
+                    }
                 }
+            }catch(Exception e)
+            {
+                System.out.println(e);
+            }
+        }
 
 
             }
