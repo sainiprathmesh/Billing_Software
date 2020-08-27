ef65224e719aedc12e26f554c6f0d4460089292b
bharti sharma <bharti.sharma_cs19@gla.ac.in>
2020-08-27 15:53:42 +0530

    actionPerformed() method is invoked

--- src/admin/RegisterEmployee.java
+++ src/admin/RegisterEmployee.java
@@ -28,6 +28,8 @@ public class RegisterEmployee extends javax.swing.JFrame {
         });
         jButton1.setText("Register");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            }
 
 
         }
