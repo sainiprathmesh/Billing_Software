510f088ea7ceeaf02e85c9cb5bf70daceace49cd
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-08-28 18:46:34 +0530

    AddItem

--- src/admin/AddItem.java
+++ src/admin/AddItem.java
@@ -87,25 +87,27 @@ class AddItem extends javax.swing.JFrame {
             jTextArea1.setColumns(20);
             jTextArea1.setRows(5);
             jScrollPane1.setViewportView(jTextArea1);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 
 
