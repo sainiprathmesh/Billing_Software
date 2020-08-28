dee00c3489b7ddd4e7a9429a48827629dcb925c5
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-08-28 19:32:17 +0530

    AddItem

--- src/admin/AddItem.java
+++ src/admin/AddItem.java
@@ -128,6 +128,7 @@ class AddItem extends javax.swing.JFrame {
                                                                                         .addComponent(jTextField3, GroupLayout.PREFERRED_SIZE, 335, GroupLayout.PREFERRED_SIZE)))
                                                                         .addGroup(GroupLayout.Alignment.TRAILING, contentPaneLayout.createSequentialGroup()
                                                                                 .addGap(97, 97, 97)
+                                                                                .addGroup(contentPaneLayout.createParallelGroup()
 
 
 
