82930d1329fed451bbe6a3ed9a2b31ec2db724c5
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-08-28 18:06:17 +0530

    AddItem

--- src/admin/AddItem.java
+++ src/admin/AddItem.java
@@ -53,6 +53,9 @@ class AddItem extends javax.swing.JFrame {
         jLabel1.setBackground(Color.white);
         jLabel1.setFont(new Font("Tahoma", Font.BOLD, 18));
         jLabel1.setForeground(new Color(255, 0, 51));
+        jLabel1.setText("Back");
+
+
 
 
 
