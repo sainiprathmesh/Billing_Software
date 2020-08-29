e9b3801efec1ce3d805a7801da8f1456de59d197
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-08-29 19:42:20 +0530

    AddItem

--- src/admin/AddItem.java
+++ src/admin/AddItem.java
@@ -297,6 +297,7 @@ class AddItem extends javax.swing.JFrame {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
 
 
 
