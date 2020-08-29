88d64fd741ba6d91b08338b262cbc7b85e8ba37d
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-08-29 19:47:35 +0530

    AddItem

--- src/admin/AddItem.java
+++ src/admin/AddItem.java
@@ -306,6 +306,7 @@ class AddItem extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(AddItem.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
             java.util.logging.Logger.getLogger(AddItem.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
 
 
 
