de51f09ebd0b7913738ab563de180f23cd4d8e6c
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-08-29 19:40:12 +0530

    AddItem

--- src/admin/AddItem.java
+++ src/admin/AddItem.java
@@ -291,6 +291,7 @@ class AddItem extends javax.swing.JFrame {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
 
 
 
