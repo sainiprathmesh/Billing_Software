40811478c824aa0b77ae66f1789fa0f5a8afba52
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-08-29 19:49:17 +0530

    AddItem

--- src/admin/AddItem.java
+++ src/admin/AddItem.java
@@ -311,6 +311,7 @@ class AddItem extends javax.swing.JFrame {
         }
         //</editor-fold>
         /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
 
 
 
