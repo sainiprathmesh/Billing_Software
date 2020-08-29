47d8e2ed4aab58fcff0f4a67dedc5016f3a9a189
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-08-29 13:45:53 +0530

    AddItem

--- src/admin/AddItem.java
+++ src/admin/AddItem.java
@@ -191,6 +191,7 @@ class AddItem extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
     private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseClicked
+        AdminPanel ap=new AdminPanel();
 
 
 
