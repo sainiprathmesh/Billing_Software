792030386d123307c9db9f13de88b5c2641a8ee8
SuryanshNaugraiya <suryansh.naugraiya_cs18@gla.ac.in>
2020-08-31 14:11:24 +0530

    called method setText()

--- src/admin/EditEmployee.java
+++ src/admin/EditEmployee.java
@@ -43,5 +43,6 @@ public class EditEmployee extends javax.swing.JFrame {
         var contentPane = getContentPane();
         jLabel1.setFont(new Font("Tahoma", Font.BOLD, 18));
         jLabel1.setForeground(new Color(255, 0, 51));
+        jLabel1.setText("Back");
     }
 }
