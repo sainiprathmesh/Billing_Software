48e0ca03e5bf1a8ad938bff42d91dda1c0435d90
SuryanshNaugraiya <suryansh.naugraiya_cs18@gla.ac.in>
2020-08-31 13:46:00 +0530

    create object jTable1

--- src/admin/EditEmployee.java
+++ src/admin/EditEmployee.java
@@ -23,5 +23,6 @@ public class EditEmployee extends javax.swing.JFrame {
     private void initComponents(){
         jLabel1 = new JLabel();
         jScrollPane1 = new JScrollPane();
+        jTable1 = new JTable();
     }
 }
