f5456fef2a65d7403bf92b465f359f120d821764
SuryanshNaugraiya <suryansh.naugraiya_cs18@gla.ac.in>
2020-08-31 22:56:17 +0530

    call setVisible() through ee and pass true as param

--- src/admin/EditEmployee.java
+++ src/admin/EditEmployee.java
@@ -229,6 +229,7 @@ public class EditEmployee extends javax.swing.JFrame {
             {
                 JOptionPane.showMessageDialog(rootPane, "Updation completed successfully");
                 EditEmployee ee=new EditEmployee();
+                ee.setVisible(true);
 
             }
             else
