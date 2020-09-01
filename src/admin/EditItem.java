00f153aa2fc58d2e677c6836a317c8da4fc26ce9
SuryanshNaugraiya <suryansh.naugraiya_cs18@gla.ac.in>
2020-09-01 13:01:26 +0530

    create object jButton2

--- src/admin/EditItem.java
+++ src/admin/EditItem.java
@@ -41,6 +41,7 @@ public class EditItem extends javax.swing.JFrame {
         jScrollPane2 = new JScrollPane();
         jTextArea1 = new JTextArea();
         jButton1 = new JButton();
+        jButton2 = new JButton();
 
 
     }
