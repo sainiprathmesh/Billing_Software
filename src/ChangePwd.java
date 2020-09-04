b0762822938bb491fbb7d86bb9ac5c11122dbf6d
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-09-04 20:00:34 +0530

    ChangePwd

--- src/ChangePwd.java
+++ src/ChangePwd.java
@@ -39,6 +39,8 @@ EmpDetails ed;
                 jLabel1.setFont(new Font("Tahoma", Font.BOLD, 18));
                 jLabel1.setForeground(new Color(255, 0, 51));
                 jLabel1.setText("Back");
+                jLabel1.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+
 
 
         }
