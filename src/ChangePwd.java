2bbb820b80fbdf45169abc870cee0ae26071a874
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-09-04 20:58:27 +0530

    ChangePwd

--- src/ChangePwd.java
+++ src/ChangePwd.java
@@ -59,6 +59,7 @@ EmpDetails ed;
                 //---- jButton1 ----
                 jButton1.setText("Change Password");
                 jButton1.addActionListener(e -> jButton1ActionPerformed(e));
+                GroupLayout contentPaneLayout = new GroupLayout(contentPane);
 
 
 
