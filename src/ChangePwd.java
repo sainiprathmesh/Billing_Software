51d829808a7081ccaa129d2bcef008fc9db397ec
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-09-04 19:17:00 +0530

    ChangePwd

--- src/ChangePwd.java
+++ src/ChangePwd.java
@@ -18,6 +18,7 @@ EmpDetails ed;
             initComponents();
             /**
              * This method is called from within the constructor to initialize the form.
+             * WARNING: Do NOT modify this code. The content of this method is always
 
 
         }
