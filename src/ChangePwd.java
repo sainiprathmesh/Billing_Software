6dae9e3127aa6a1606018ac2319c3d54e9997eb5
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-09-05 18:33:21 +0530

    ChangePwd

--- src/ChangePwd.java
+++ src/ChangePwd.java
@@ -158,6 +158,7 @@ public static void main(String args[]) {
         Logger.getLogger(employee.ChangePwd.class.getName()).log(Level.SEVERE, null, ex);
     } catch (IllegalAccessException ex) {
         Logger.getLogger(employee.ChangePwd.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (javax.swing.UnsupportedLookAndFeelException ex) {
 
 
 
