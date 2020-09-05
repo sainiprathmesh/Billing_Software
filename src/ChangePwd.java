3a59acb14752920012d009a3c8b1132f89893f03
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-09-05 18:28:27 +0530

    ChangePwd

--- src/ChangePwd.java
+++ src/ChangePwd.java
@@ -142,7 +142,9 @@ EmpDetails ed;
 public static void main(String args[]) {
     /* Set the Nimbus look and feel */
     //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-
+    /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+     * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+     */
 
 
 
