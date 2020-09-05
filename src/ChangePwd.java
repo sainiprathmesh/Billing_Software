97f6c91938a59ffafbc2bdd86bf6247e0f1e9ad1
Sonal Singh <sonal.singh_cs18@gla.ac.in>
2020-09-05 18:30:01 +0530

    ChangePwd

--- src/ChangePwd.java
+++ src/ChangePwd.java
@@ -147,6 +147,7 @@ public static void main(String args[]) {
      */
     try {
         for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
 
 
 
