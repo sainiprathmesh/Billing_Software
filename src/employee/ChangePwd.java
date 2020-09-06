d55f41a1a13c0de738f24ac180d69a05f388fd42
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-09-06 23:59:04 +0530

    element variable declared.

--- src/employee/ChangePwd.java
+++ src/employee/ChangePwd.java
@@ -1,7 +1,14 @@
 package employee;
 
+import javax.swing.*;
+
 /**
  * @author prathmeshkumarsaini on 06/09/20 at 11:57 PM
  */
 public class ChangePwd extends javax.swing.JFrame {
+    private JLabel jLabel1;
+    private JTextField jTextField1;
+    private JPasswordField jPasswordField1;
+    private JPasswordField jPasswordField2;
+    private JButton jButton1;
 }
