276d8d0241474958d4233ff76111463c33c11b5e
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-09-06 23:49:31 +0530

    object created for various element.

--- src/admin/AddItem.java
+++ src/admin/AddItem.java
@@ -1,5 +1,6 @@
 package admin;
 
+import javax.swing.*;
 import java.io.File;
 
 /**
@@ -12,8 +13,40 @@ public class AddItem extends javax.swing.JFrame {
         initComponents();
     }
 
+    private JLabel jLabel1;
+    private JLabel jLabel2;
+    private JTextField jTextField1;
+    private JLabel jLabel3;
+    private JTextField jTextField2;
+    private JLabel jLabel4;
+    private JTextField jTextField3;
+    private JLabel jLabel5;
+    private JComboBox<String> jComboBox1;
+    private JLabel jLabel6;
+    private JScrollPane jScrollPane1;
+    private JTextArea jTextArea1;
+    private JLabel jLabel7;
+    private JButton jButton1;
+    private JLabel jLabel8;
+    private JButton jButton2;
+
     @SuppressWarnings("unchecked")
     private void initComponents() {
-
+        jLabel1 = new JLabel();
+        jLabel2 = new JLabel();
+        jTextField1 = new JTextField();
+        jLabel3 = new JLabel();
+        jTextField2 = new JTextField();
+        jLabel4 = new JLabel();
+        jTextField3 = new JTextField();
+        jLabel5 = new JLabel();
+        jComboBox1 = new JComboBox<>();
+        jLabel6 = new JLabel();
+        jScrollPane1 = new JScrollPane();
+        jTextArea1 = new JTextArea();
+        jLabel7 = new JLabel();
+        jButton1 = new JButton();
+        jLabel8 = new JLabel();
+        jButton2 = new JButton();
     }
 }
