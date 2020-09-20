21b6ad43b0a3ec8fda991e53ebff6850184a86b1
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-09-20 19:24:30 +0530

    define image varible and initialize it to null

--- src/employee/CartList.java
+++ src/employee/CartList.java
@@ -50,7 +50,7 @@ public class CartList extends javax.swing.JFrame {
         select_itemid = (String) jTable1.getValueAt(row, 1);
         quantity = (String) jTable1.getValueAt(row, 4);
         String id, name = null, price = null, desc = null, category = null;
-//        byte[] image = null;
+        byte[] image = null;
 //        try {
 //            Connection con = dbconnection.DbConnection.getConnect();
 //            PreparedStatement ps = con.prepareStatement("select * from items where id='" + select_itemid + "'");
