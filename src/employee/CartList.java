7e56c8d685139f0dfca849e9e5f5a1fe88594214
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-09-20 18:24:12 +0530

    define ed variabe of EmpDetails

--- src/employee/CartList.java
+++ src/employee/CartList.java
@@ -1,7 +1,11 @@
 package employee;
 
+import model.EmpDetails;
+
 public class CartList extends javax.swing.JFrame {
     int row;
     String select_itemid, quantity;
+    EmpDetails ed;
+//    HashMap<Integer, ArrayList> hm;
 
 }
