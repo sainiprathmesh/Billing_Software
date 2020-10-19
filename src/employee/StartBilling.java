1805dc37a7904c615819d9df9cd4b4b8f759fb66
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-10-19 16:51:25 +0530

    create method jTextField1KeyReleased()

--- src/employee/StartBilling.java
+++ src/employee/StartBilling.java
@@ -240,7 +240,11 @@ public class StartBilling extends javax.swing.JFrame {
     private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {
         EmpPanel ep = new EmpPanel(ed);
         ep.setVisible(true);
-//        setVisible(false);
+        setVisible(false);
+    }
+
+    private void jTextField1KeyReleased(java.awt.event.KeyEvent evt) {
+
     }
 
 
