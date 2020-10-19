0848f152be6c279275d35e6633463d8852f9d589
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-10-19 15:36:34 +0530

    create ArrayList instance variable 'al'.

--- src/employee/StartBilling.java
+++ src/employee/StartBilling.java
@@ -1,6 +1,11 @@
 package employee;
 
+import java.util.ArrayList;
+
 public class StartBilling extends javax.swing.JFrame {
+    ArrayList al;
+//    HashMap<Integer, ArrayList> hm;
+//    int count=0;
 
 
 }
