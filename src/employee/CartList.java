7c515f19b5b8973ed8facc83d3b5b8d411769bb0
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-10-19 18:00:41 +0530

    StartBilling completed and tested sucessfully.

--- src/employee/CartList.java
+++ src/employee/CartList.java
@@ -5,6 +5,10 @@ import model.EmpDetails;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -23,7 +27,31 @@ public class CartList extends javax.swing.JFrame {
         this.ed = ed;
         this.hm = hm;
 
-//        initComponents();
+        initComponents();
+    }
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(CartList.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(CartList.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(CartList.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(CartList.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new CartList(null, null).setVisible(true);
+            }
+        });
     }
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {
@@ -150,11 +178,145 @@ public class CartList extends javax.swing.JFrame {
 
     }
 
-    private void jLabel4MouseClicked(java.awt.event.MouseEvent evt) {
-//        StartBilling sb = new StartBilling(ed, hm);
-//        sb.setVisible(true);
-//        setVisible(false);
+    private void initComponents() {
+        jScrollPane1 = new JScrollPane();
+        jTable1 = new JTable();
+        jLabel1 = new JLabel();
+        jLabel2 = new JLabel();
+        jLabel3 = new JLabel();
+        jComboBox1 = new JComboBox<>();
+        jButton1 = new JButton();
+        jButton2 = new JButton();
+        jLabel4 = new JLabel();
 
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                formWindowOpened(e);
+            }
+        });
+        var contentPane = getContentPane();
+
+
+        {
+
+            jTable1.setModel(new DefaultTableModel(
+                    new Object[][]{
+                    },
+                    new String[]{
+                            "Sr.No", "Item ID", "Item Name", "Item Price", "Item Quantity", "Total Price"
+                    }
+            ) {
+                boolean[] columnEditable = new boolean[]{
+                        false, false, false, false, false, false
+                };
+
+                @Override
+                public boolean isCellEditable(int rowIndex, int columnIndex) {
+                    return columnEditable[columnIndex];
+                }
+            });
+            jTable1.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    jTable1MouseClicked(e);
+                }
+            });
+            jScrollPane1.setViewportView(jTable1);
+        }
+
+        jLabel1.setText("Total Price :");
+
+        jLabel2.setText("________________");
+
+        jComboBox1.setModel(new DefaultComboBoxModel<>(new String[]{
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10"
+        }));
+
+        jButton1.setText("Update");
+        jButton1.addActionListener(e -> jButton1ActionPerformed(e));
+
+        jButton2.setText("Delete");
+        jButton2.addActionListener(e -> jButton2ActionPerformed(e));
+
+        jLabel4.setFont(new Font("Tahoma", Font.BOLD, 18));
+        jLabel4.setForeground(Color.red);
+        jLabel4.setText("Back");
+        jLabel4.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        jLabel4.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                jLabel4MouseClicked(e);
+            }
+        });
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+                contentPaneLayout.createParallelGroup()
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                .addGroup(contentPaneLayout.createParallelGroup()
+                                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                                .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.TRAILING, false)
+                                                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                                                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                                .addComponent(jLabel1)
+                                                                .addGap(18, 18, 18)
+                                                                .addComponent(jLabel2))
+                                                        .addGroup(GroupLayout.Alignment.LEADING, contentPaneLayout.createSequentialGroup()
+                                                                .addGap(73, 73, 73)
+                                                                .addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 646, GroupLayout.PREFERRED_SIZE)))
+                                                .addGroup(contentPaneLayout.createParallelGroup()
+                                                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                                                .addGap(189, 189, 189)
+                                                                .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                                                        .addComponent(jLabel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                                        .addComponent(jComboBox1, GroupLayout.DEFAULT_SIZE, 205, Short.MAX_VALUE)))
+                                                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                                                .addGap(181, 181, 181)
+                                                                .addComponent(jButton1, GroupLayout.PREFERRED_SIZE, 102, GroupLayout.PREFERRED_SIZE)
+                                                                .addGap(94, 94, 94)
+                                                                .addComponent(jButton2, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE))))
+                                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                                .addGap(33, 33, 33)
+                                                .addComponent(jLabel4)))
+                                .addContainerGap(144, Short.MAX_VALUE))
+        );
+        contentPaneLayout.setVerticalGroup(
+                contentPaneLayout.createParallelGroup()
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel4)
+                                .addGap(33, 33, 33)
+                                .addGroup(contentPaneLayout.createParallelGroup()
+                                        .addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                                .addGap(11, 11, 11)
+                                                .addComponent(jLabel3, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE)
+                                                .addGap(47, 47, 47)
+                                                .addComponent(jComboBox1, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)
+                                                .addGap(86, 86, 86)
+                                                .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                                                        .addComponent(jButton1, GroupLayout.DEFAULT_SIZE, 47, Short.MAX_VALUE)
+                                                        .addComponent(jButton2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                                .addGap(28, 28, 28)
+                                .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jLabel1)
+                                        .addComponent(jLabel2))
+                                .addContainerGap(218, Short.MAX_VALUE))
+        );
+        pack();
+        setLocationRelativeTo(null);
     }
 
     private JTable jTable1;
@@ -167,11 +329,10 @@ public class CartList extends javax.swing.JFrame {
     private JLabel jLabel1;
     private JScrollPane jScrollPane1;
 
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new CartList(null, null).setVisible(true);
-            }
-        });
+    private void jLabel4MouseClicked(java.awt.event.MouseEvent evt) {
+        StartBilling sb = new StartBilling(ed, hm);
+        sb.setVisible(true);
+        setVisible(false);
+
     }
 }
