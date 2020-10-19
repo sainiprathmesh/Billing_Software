a97a11a2c3de327a4156aa56ed5ba719873f2384
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-10-19 15:54:56 +0530

    create private instance variable jLable1,2,3,4,5,6,7,8,9,10,11,12.

--- src/employee/StartBilling.java
+++ src/employee/StartBilling.java
@@ -2,6 +2,7 @@ package employee;
 
 import model.EmpDetails;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -10,5 +11,30 @@ public class StartBilling extends javax.swing.JFrame {
     HashMap<Integer, ArrayList> hm;
     int count = 0;
     EmpDetails ed;
+
+    private JLabel jLabel1;
+    //    private JTextField jTextField1;
+    private JLabel jLabel2;
+    private JLabel jLabel3;
+    private JLabel jLabel4;
+    private JLabel jLabel5;
+    private JLabel jLabel6;
+    private JLabel jLabel7;
+    private JLabel jLabel8;
+    private JLabel jLabel9;
+    private JLabel jLabel10;
+    private JLabel jLabel12;
+
+    public StartBilling(EmpDetails ed, HashMap hm) {
+//        initComponents();
+//        jButton1.setEnabled(false);
+//        jComboBox1.setEnabled(false);
+    }
+//    private JScrollPane jScrollPane1;
+//    private JTextArea jTextArea1;
+//    private JButton jButton1;
+//    private JComboBox<String> jComboBox1;
+//    private JLabel jLabel11;
+//    private JButton jButton2;
 
 }
