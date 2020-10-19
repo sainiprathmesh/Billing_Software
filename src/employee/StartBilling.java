176c789d0565d439f63ad1589845aed60ff10ef3
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-10-19 15:38:04 +0530

    create instance variable count and initialize

--- src/employee/StartBilling.java
+++ src/employee/StartBilling.java
@@ -1,11 +1,12 @@
 package employee;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class StartBilling extends javax.swing.JFrame {
     ArrayList al;
-//    HashMap<Integer, ArrayList> hm;
-//    int count=0;
+    HashMap<Integer, ArrayList> hm;
+    int count = 0;
 
 
 }
