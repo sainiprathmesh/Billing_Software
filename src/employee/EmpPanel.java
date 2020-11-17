8f4efa6e253aa0881d5a5094b5f0d77f0781fd0d
Prathmesh Kumar Saini <prathmesh.saini_cs18@gla.ac.in>
2020-11-17 22:01:08 +0530

    Start Billing Activated.

--- src/employee/EmpPanel.java
+++ src/employee/EmpPanel.java
@@ -128,8 +128,8 @@ public class EmpPanel extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
 
         HashMap hm = new HashMap();
-        // StartBilling sb=new StartBilling(ed, hm);
-        //sb.setVisible(true);
+        StartBilling sb = new StartBilling(ed, hm);
+        sb.setVisible(true);
         setVisible(false);
 
     }
